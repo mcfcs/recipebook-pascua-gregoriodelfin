159e60d5a510c99dd17eac90ca1f776ba67c9bdf
mcfc <gregoriodelfinpascua@gmail.com>
2025-02-24 02:22:06 +0800

    Removed unecessary path in urls.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . views import basicParams
 
 urlpatterns = [
-    path('', basicParams, name='basicParams'),
     path("recipe/<str:id>/", basicParams, name="basicParams"),
     path("recipes/list/", basicParams, name="basicParams")
 ]
