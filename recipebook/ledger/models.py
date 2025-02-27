834fe82485b5e000746fbb987e28d5b7203f0a0c
mcfc <gregoriodelfinpascua@gmail.com>
2025-02-27 21:07:32 +0800

    Initialized model classes

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -2,4 +2,11 @@ from datetime import datetime
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    pass
+
+class Recipe(models.Model):
+    pass
+
+class RecipeIngredient(models.Model):
+    pass
