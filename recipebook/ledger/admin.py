aba2bc9bc9e2f0bf7eabf18e9558163f87851f16
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-04 09:08:23 +0800

    Initialized admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 from .models import Recipe, RecipeIngredient, Ingredient
 
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ("id", "Recipe", "Ingredient", "Quantity")
+    search_fields = ("Recipe__name", "Ingredient__name")
+    list_filter = ("Recipe", "Ingredient")
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
