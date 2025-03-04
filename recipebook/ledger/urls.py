153ba3f333051076bec4b40413144d2538d08706
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-04 10:35:00 +0800

    Changed str:id to int:pk

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("recipe/<str:id>/", RecipeView.as_view(), name="recipe-detail"),
+    path("recipe/<int:pk>/", RecipeView.as_view(), name="recipe-detail"),
     path("recipes/list/", RecipeListView.as_view(), name="recipe-list")
 ]
 
