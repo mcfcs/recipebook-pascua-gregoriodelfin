c5d58860d6fffe6061d98d8094a9e561c4edd08f
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-04 12:09:04 +0800

    Added brief docstrings to models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,26 +1,38 @@
-from datetime import datetime
+"""
+@brief Models for Recipe app.
+"""
+
 from django.db import models
 from django.urls import reverse
 
 class Ingredient(models.Model):
+    """
+    @brief Instantiates Ingredient model.
+    """
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('ingredient_detail', args=[str(self.name)])
 
 class Recipe(models.Model):
+    """
+    @brief Instantiates the Recipe model
+    """
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('recipe_detail', args=[str(self.name)])
 
 class RecipeIngredient(models.Model):
+    """
+    @brief Links Recipe & Ingredient Models.
+    """
     Quantity = models.CharField(max_length=100)
     Ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="ingredient")
     Recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="recipe")
