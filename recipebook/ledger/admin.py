34bb97114aa0fc5d2088eb707ca69d2d3098bd68
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-04 12:05:47 +0800

    Added brief class and module docs

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,15 +1,28 @@
+"""
+This module registers the models Recipe, RecipeIngredient, and Ingredient
+"""
+
 from django.contrib import admin
 from .models import Recipe, RecipeIngredient, Ingredient
 
 class IngredientAdmin(admin.ModelAdmin):
+    """
+    @brief Admin configuration for the Ingredient model.
+    """
     list_display = ("id", "name")
     search_fields = ("name",)
 
 class RecipeAdmin(admin.ModelAdmin):
+    """
+    @brief Admin configuration for the Recipe model.
+    """
     list_display = ("id", "name")
     search_fields = ("name",)
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
+    """
+    @brief Admin configuration for the RecipeIngredient model.
+    """
     list_display = ("id", "Recipe", "Ingredient", "Quantity")
     search_fields = ("Recipe__name", "Ingredient__name")
     list_filter = ("Recipe", "Ingredient")
