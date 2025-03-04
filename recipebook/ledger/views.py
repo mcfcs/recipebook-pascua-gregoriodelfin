81e0cacdb4b19c4d6a4e4d8adf4a052d323dc46b
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-04 09:35:53 +0800

    Updated views.py to use models

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,83 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import *
+from .models import Recipe
 
-content = {
-    "recipes": [
-        {
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "/recipe/1"
-        },
-        {
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "/recipe/2"
-        }
-    ]
-}
+class TaskListView(ListView):
+    model = Recipe
+    template_name = 'recipehome.html'
 
-def basic_params(request, id="list"):
-    '''     
-    @fn basic_params
-    @brief returns the proper html site and
-    context from a given id.
-
-    '''
-    if id == "1":
-        ctx = content["recipes"][0]
-        return render(request, 'recipe.html', ctx)
-
-    if id == "2":
-        ctx = content["recipes"][1]
-        return render(request, 'recipe.html', ctx)
-
-    return render(request, 'recipehome.html', content)
+class TaskDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe.html'
