71134f0604e51e476a28aa7d91a142a9eb6a5f71
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-04 09:39:43 +0800

    Updated urls.py to use the classes in views.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . views import basic_params
+from .views import *
 
 urlpatterns = [
-    path("recipe/<str:id>/", basic_params, name="basic_params"),
-    path("recipes/list/", basic_params, name="basic_params")
+    path("recipe/<str:id>/", RecipeView.as_view(), name="recipe-detail"),
+    path("recipes/list/", RecipeListView.as_view(), name="recipe-list")
 ]
+
