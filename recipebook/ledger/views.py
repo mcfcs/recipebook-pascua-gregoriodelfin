159700ac1db4f3bf50fa0a8375fc1b39891a9721
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-18 22:03:12 +0800

    Added Login Required to views

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -3,12 +3,15 @@ This module registers the views of the django html
 """
 
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipehome.html'
+    redirect_field_name = "registration/login.html"
 
-class RecipeView(DetailView):
+class RecipeView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe.html'
+    redirect_field_name = "registration/login.html"
