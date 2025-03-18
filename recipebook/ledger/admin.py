b1094d6e774dae7f5098fc66b3707f89a59e201b
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-18 21:55:41 +0800

    Fixed admin.py for ledger app

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -3,9 +3,7 @@ This module registers the models Recipe, RecipeIngredient, and Ingredient
 """
 
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
-from .models import Recipe, RecipeIngredient, Ingredient, Profile
+from .models import Recipe, RecipeIngredient, Ingredient
 
 class RecipeAdmin(admin.ModelAdmin):
     """
@@ -25,15 +23,6 @@ class IngredientAdmin(admin.ModelAdmin):
     """
     model = Ingredient
 
-class ProfileInline(admin.StackedInline):
-    model = Profile
-    can_delete = False
-
-class UserAdmin(BaseUserAdmin):
-    inlines = [ProfileInline,]
-
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
