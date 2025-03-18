9b796125406cae0fbb3e1aa06224f3f93bc2bdad
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-18 22:04:01 +0800

    Removed accounts path in ledger app

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,5 +4,4 @@ from .views import *
 urlpatterns = [
     path("recipe/<int:pk>/", RecipeView.as_view(), name="recipe-detail"),
     path("recipes/list/", RecipeListView.as_view(), name="recipe-list"),
-    path('/accounts', include('django.contrib.auth.urls'))
 ]
