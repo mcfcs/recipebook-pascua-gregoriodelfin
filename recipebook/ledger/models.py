fd153c29c7659a6a0c343d9bb008410c0aaeb746
mcfc <gregoriodelfinpascua@gmail.com>
2025-03-18 22:19:49 +0800

    Returned name for RecipeIngredient model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -40,3 +40,6 @@ class RecipeIngredient(models.Model):
     Quantity = models.CharField(max_length=100)
     Ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="ingredient")
     Recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="recipe")
+
+    def __str__(self):
+        return f"{self.Recipe}: {self.Ingredient}, {self.Quantity}"
