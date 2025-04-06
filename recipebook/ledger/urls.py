a8950dd0bcaf2fc9fbebf0ad5410c17a91d63b48
mcfc <gregoriodelfinpascua@gmail.com>
2025-04-07 03:29:38 +0800

    Added urls add and add_image

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from .views import *
 
 urlpatterns = [
     path("recipe/<int:pk>/", RecipeView.as_view(), name="recipe-detail"),
     path("recipes/list/", RecipeListView.as_view(), name="recipe-list"),
+    path("recipe/add/", RecipeCreateView.as_view(), name="recipe-add"),
+    path("recipe/<int:pk>/add_image", RecipeUpdateDetail.as_view(), name="recipe-add-image"),
 ]
